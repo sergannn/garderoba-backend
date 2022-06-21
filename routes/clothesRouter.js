9f08c0cdd1610cb1d386411016202e36b44b0117
Angela Herrig <angela.k.herrig@gmail.com>
2022-06-21 09:09:54 +0200

    Homescreen add images to boxes

--- routes/clothesRouter.js
+++ routes/clothesRouter.js
@@ -10,9 +10,40 @@ function clothProcessor(presentWeather) {
 }
 
 // search endpoint
-clothesRouter.get("/", async (req, res, next) => {
+clothesRouter.get("/closet", async (req, res, next) => {
   // this is supposed to find all the clothes of a user.
   try {
+    const clothes = await Cloth.find(); //we are sending all clothes from this
+    res.send(clothes);
+  } catch (error) {
+    next({
+      status: 401,
+      message: error.message,
+      originalError: error,
+    });
+  }
+});
+
+clothesRouter.get("/favorite", async (req, res, next) => {
+  // this is supposed to find all the clothes of a user.
+  try {
+    const clothes = await Cloth.find(); //we are sending all clothes from this
+    res.send(clothes);
+  } catch (error) {
+    next({
+      status: 401,
+      message: error.message,
+      originalError: error,
+    });
+  }
+});
+clothesRouter.get("/home", async (req, res, next) => {
+  // this is supposed to find all the clothes of a user.
+  try {
+    const clothesTopBox = await Cloth.find({ type: "top" }); //we are sending all clothes from this
+    const clothesBottomBox = await Cloth.find({ type: "bottom" }); //we are sending all clothes from this
+
+    res.send({ clothesTopBox, clothesBottomBox });
   } catch (error) {
     next({
       status: 401,
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,47 +1,51 @@
-import express from 'express'
-import multer from 'multer'
-import Cloth from "../models/Cloth.js"
-import path from "path"
+import express from "express";
+import multer from "multer";
+import Cloth from "../models/Cloth.js";
+import path from "path";
 
-
-const uploadRouter = express.Router()
+const uploadRouter = express.Router();
 // const uploadAPic = multer({ dest: "uploads/", limits: { fileSize: 1024 *1024 }  });
 
 // const handleUpload = uploadAPic.fields([{ name: "selectedImage", maxCount: 1 }]);
 
-uploadRouter.post("/", async(req,res,next)=>{
-    // console.log(req.files);
-    console.log(req.body);
+uploadRouter.post("/", async (req, res, next) => {
+  // console.log(req.files);
+  console.log(req.body);
 
-    try {
-        const cloth = await Cloth.create(req.body)
-        console.log(cloth);
-        res.send(cloth)
-    } catch (error) {
-        next({
-            status: 400, 
-            message: error.message,
-            originalError: error
-        })
+  try {
+    const cloth = await Cloth.create(req.body);
+
+    if (cloth.type === "top") {
+      const clothTopBox = await Cloth.find({ type: "top" });
+      res.send(clothTopBox);
+      console.log(clothTopBox);
+    } else {
+      const clothBottomBox = await Cloth.find({ type: "bottom" });
+      res.send(clothBottomBox);
     }
-})
+    console.log(cloth);
+    res.send(cloth);
+  } catch (error) {
+    next({
+      status: 400,
+      message: error.message,
+      originalError: error,
+    });
+  }
+});
 
-uploadRouter.get("/", async(req,res,next)=>{
-    
-    try {
-       
-        const cloth = await Cloth.findById("62ac68d5f321765b93ec8c02")
-        
+uploadRouter.get("/", async (req, res, next) => {
+  try {
+    const cloth = await Cloth.findById("62ac68d5f321765b93ec8c02");
 
-        res.send(cloth)
+    res.send(cloth);
+  } catch (error) {
+    next({
+      status: 401,
+      message: error.message,
+      originalError: error,
+    });
+  }
+});
 
-    } catch (error) {
-        next({
-            status: 401, 
-            message: error.message,
-            originalError: error
-        })
-    }
-})
-
-export default uploadRouter
+export default uploadRouter;
