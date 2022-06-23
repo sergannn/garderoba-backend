e7f6f805fd04edd9b7920a1feeef1ce0e4d8a6c9
Angela Herrig <angela.k.herrig@gmail.com>
2022-06-23 14:41:52 +0200

    endpoint query style

--- routes/clothesRouter.js
+++ routes/clothesRouter.js
@@ -13,8 +13,14 @@ const clothesRouter = express.Router();
 clothesRouter.get("/closet", async (req, res, next) => {
   // this is supposed to find all the clothes of a user.
   try {
-    const clothes = await Cloth.find(); //we are sending all clothes from this
-    res.send(clothes);
+    if (Object.keys(req.query).length === 0) {
+      const clothes = await Cloth.find(); //we are sending all clothes from this
+
+      res.send(clothes);
+    } else {
+      const clothes = await Cloth.find(req.query);
+      res.send(clothes);
+    }
   } catch (error) {
     next({
       status: 401,
@@ -42,8 +48,8 @@ clothesRouter.get("/home", async (req, res, next) => {
   try {
     const clothesTopBox = await Cloth.find({ type: "top" }); //we are sending all clothes from this
     const clothesBottomBox = await Cloth.find({ type: "bottom" }); //we are sending all clothes from this
-    const favorites = await Cloth.find({favorite:true})
-    
+    const favorites = await Cloth.find({ favorite: true });
+
     clothesTopBox.reverse();
     clothesBottomBox.reverse();
     res.send({ clothesTopBox, clothesBottomBox, favorites });
@@ -57,22 +63,20 @@ clothesRouter.get("/home", async (req, res, next) => {
 });
 
 clothesRouter.put("/:id", async (req, res, next) => {
-
   console.log("req here:", req.body);
   try {
     const id = req.params.id;
     console.log("id", id);
     const cloth = await Cloth.findById(id);
-    cloth.favorite = req.body.favorite
+    cloth.favorite = req.body.favorite;
     cloth.save();
-    
+
     res.send(cloth);
     if (!cloth) {
-      return next({ status: 404, message: "not found"});
+      return next({ status: 404, message: "not found" });
     }
-  
   } catch (error) {
-    next({ status: 400, message: error.message});
+    next({ status: 400, message: error.message });
   }
 
   //  remove add favorite, edit the season. for all kinds of update
