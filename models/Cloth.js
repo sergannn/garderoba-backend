8635212a0431b8ad8113d1a8e12d7f6b7a1c345d
Angela Herrig <angela.k.herrig@gmail.com>
2022-07-07 14:49:19 +0200

    updated clothSchema

--- models/Cloth.js
+++ models/Cloth.js
@@ -9,31 +9,40 @@ const trim = true;
 
 const clothSchema = new mongoose.Schema(
   {
-    user:       { type: Schema.Types.ObjectId, ref: "user" },
-    image:      { type: String },
-    favorite:   { type: Boolean, default: false },
-    type:       { type: String, enum: ["top", "bottom", "full"] },
-    color:      { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "user" },
+    image: { type: String },
+    favorite: { type: Boolean, default: false },
+    type: { type: String, enum: ["top", "bottom", "full"] },
+    color: { type: String },
     // season:     { type: String, enum: ["summer", "winter", "in-between"], required},
-    season:     { type: [String], required},
-    style:      { type: String, enum: ["casual", "formal", "work", "sports", "night-out","lounge-wear"] },
+    season: { type: [String], required },
+    style: {
+      type: String,
+      enum: [
+        "casual",
+        "formal",
+        "work",
+        "sports",
+        "night-out",
+        "lounge-wear",
+        "rainy",
+      ],
+    },
   },
   { timestamps }
 );
 
-clothSchema.pre("remove", async function(){
+clothSchema.pre("remove", async function () {
+  const id = this._id.toString();
+  console.log("Cloth is being removed " + id);
 
-  const id = this._id.toString()
-  console.log("Cloth is being removed " + id)
+  const user = await User.findById(this.user);
 
-  const user = await User.findById(this.user)
-
-  if(user){
-    user.clothes = user.clothes.filter(x=> x.toString() != id)
-    await user.save()
+  if (user) {
+    user.clothes = user.clothes.filter((x) => x.toString() != id);
+    await user.save();
   }
-})
-
+});
 
 const Cloth = model("cloth", clothSchema);
 export default Cloth;
